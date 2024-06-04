6926bad5301c49b78318c9e7e32d65534c29a08c
Dhrutinandan <dhrutiswainnandan@gmail.com>
2024-06-04 20:37:04 +0530

    feat: add marquee effect to team and sponsor carousel

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -1,36 +1,37 @@
-
-import {useAnimate } from 'framer-motion';
-import { useEffect, useState, useCallback } from 'react';
+// import {useAnimate } from 'framer-motion';
+// import { useEffect, useState, useCallback } from 'react';
+import Marquee from 'react-fast-marquee';
 
 export default function Sponsors() {
 
-    const [scope, animate] = useAnimate();
-  const [isAnimating] = useState(true);
+  //   const [scope, animate] = useAnimate();
+  // const [isAnimating] = useState(true);
 
-  const handleAnimation = useCallback(async () => {
-    while (isAnimating) {
-        await animate('#ani', { x: -70 }, { delay: 0.2, duration: 0.6, ease: [0.6, 0.01, -0.05, 0.9] });
-        await animate('#ani', { x: -240 }, { delay: 0.2, duration: 0.6, ease: [0.6, 0.01, -0.05, 0.9] });
-        await animate('#ani', { x: -350 }, { delay: 0.2, duration: 0.6, ease: [0.6, 0.01, -0.05, 0.9] });
-        await animate('#ani', { x: 0 }, { delay: 0.2, duration: 0.6, ease: [0.6, 0.01, -0.05, 0.9] });
-      }
-  }, [animate, isAnimating]);
+  // const handleAnimation = useCallback(async () => {
+  //   while (isAnimating) {
+  //       await animate('#ani', { x: -70 }, { delay: 0.2, duration: 0.6, ease: [0.6, 0.01, -0.05, 0.9] });
+  //       await animate('#ani', { x: -240 }, { delay: 0.2, duration: 0.6, ease: [0.6, 0.01, -0.05, 0.9] });
+  //       await animate('#ani', { x: -350 }, { delay: 0.2, duration: 0.6, ease: [0.6, 0.01, -0.05, 0.9] });
+  //       await animate('#ani', { x: 0 }, { delay: 0.2, duration: 0.6, ease: [0.6, 0.01, -0.05, 0.9] });
+  //     }
+  // }, [animate, isAnimating]);
   
-  useEffect(() => {
-    handleAnimation();
-  }, [handleAnimation]);
+  // useEffect(() => {
+  //   handleAnimation();
+  // }, [handleAnimation]);
 
     return (
-        <div className="bg-brown-2 h-[411px] pt-12">
+        <div className="bg-brown-2 h-[211px] md:h-[411px] pt-12">
             
-                <div ref={scope} className="grid grid-flow-col gap-36 items-center overflow-x-auto no-scrollbar pb-4">
-                    <div id='ani' className=" w-[253px] h-[45px] bg-icon_1 bg-cover h-200px"></div>
-                    <div id='ani' className="w-[253px] h-[50px] bg-icon_2 bg-cover"></div>
-                    <div id='ani' className="w-[175px] h-[38px] bg-icon_3 bg-cover"></div>
-                    <div id='ani' className="w-[80px] h-[80px] bg-icon_4 bg-cover"></div>
-                    <div id='ani' className="w-[180px] h-[45px] bg-icon_5 bg-cover"></div>
-                    <div id='ani' className="w-[120px] h-[50px] bg-icon_6 bg-cover"></div>
-                </div>
+                <Marquee>
+                    <div id='ani' className=" w-[253px] h-[45px] bg-icon_1 bg-cover mr-6 md:mr-24"></div>
+                    <div id='ani' className="w-[253px] h-[50px] bg-icon_2 bg-cover mr-6 md:mr-24"></div>
+                    <div id='ani' className="w-[175px] h-[38px] bg-icon_3 bg-cover mr-6 md:mr-24"></div>
+                    <div id='ani' className="w-[80px] h-[80px] bg-icon_4 bg-cover mr-6 md:mr-24"></div>
+                    <div id='ani' className="w-[180px] h-[45px] bg-icon_5 bg-cover mr-6 md:mr-24"></div>
+                    <div id='ani' className="w-[120px] h-[50px] bg-icon_6 bg-cover mr-6 md:mr-24"></div>
+                    <div id='ani' className="w-[175px] h-[38px] bg-icon_3 bg-cover mr-6 md:mr-24"></div>
+                </Marquee>
             
         </div>
     )
--- src/components/TeamCarousel.jsx
+++ src/components/TeamCarousel.jsx
@@ -1,70 +1,68 @@
-import { motion, useAnimate } from "framer-motion";
-import { useEffect, useState, useCallback } from "react";
+// import { motion, useAnimate } from "framer-motion";
+// import { useEffect, useState, useCallback } from "react";
 import { teamMembers}  from "./Team_Sponsors_Cards";
+import Marquee from "react-fast-marquee";
 
 export default function TeamCard() {
-  const [scope, animate] = useAnimate();
-  const [isAnimating] = useState(true);
+  // const [scope, animate] = useAnimate();
+  // const [isAnimating] = useState(true);
 
-  const handleAnimation = useCallback(async () => {
-    while (isAnimating) {
-      await animate(
-        "#ani",
-        { x: -337 },
-        { delay: 0.7, duration: 1, type: "spring", stiffness: 450 }
-      );
-      await animate(
-        "#ani",
-        { x: -685 },
-        { delay: 0.7, duration: 1, type: "spring", stiffness: 450 }
-      );
-      await animate(
-        "#ani",
-        { x: -1035 },
-        { delay: 0.7, duration: 1, type: "spring", stiffness: 450 }
-      );
-      await animate(
-        "#ani",
-        { x: 0 },
-        { delay: 0.7, duration: 1, type: "spring", stiffness: 450 }
-      );
-    }
-  }, [animate, isAnimating]);
+  // const handleAnimation = useCallback(async () => {
+  //   while (isAnimating) {
+  //     await animate(
+  //       "#ani",
+  //       { x: -337 },
+  //       { delay: 0.7, duration: 1, type: "spring", stiffness: 450 }
+  //     );
+  //     await animate(
+  //       "#ani",
+  //       { x: -685 },
+  //       { delay: 0.7, duration: 1, type: "spring", stiffness: 450 }
+  //     );
+  //     await animate(
+  //       "#ani",
+  //       { x: -1035 },
+  //       { delay: 0.7, duration: 1, type: "spring", stiffness: 450 }
+  //     );
+  //     await animate(
+  //       "#ani",
+  //       { x: 0 },
+  //       { delay: 0.7, duration: 1, type: "spring", stiffness: 450 }
+  //     );
+  //   }
+  // }, [animate, isAnimating]);
 
-  useEffect(() => {
-    handleAnimation();
-  }, [handleAnimation]);
+  // useEffect(() => {
+  //   handleAnimation();
+  // }, [handleAnimation]);
 
   return (
-    <div className="bg-brown-2 h-[700px]">
-      <div className="font-poppins text-brown-4 text-[128px] font-bold">
+    <div className="bg-brown-2 md:h-[700px]">
+      <div className="font-poppins text-brown-4 text-[48px] font-bold md:text-[128px]">
         Meet the team
       </div>
 
-      <motion.div
-        ref={scope}
-        className="grid grid-flow-col gap-4 overflow-x-auto no-scrollbar pb-4"
-      >
+      <Marquee className="grid grid-flow-col gap-4 overflow-x-auto no-scrollbar pb-4 gradie===">
           {teamMembers.map((member, index) => (
             <div
               key={index}
               id="ani"
-              className="bg-brown-5 h-[422px] w-[332px] flex flex-col items-center shadow-customShadow rounded-[16px] mr-2"
+              className="bg-brown-5 h-[322px] w-[232px] md:h-[422px] md:w-[332px] flex flex-col items-center shadow-customShadow rounded-[16px] mr-3"
             >
               <div
-                className={`h-[240px] w-[240px] mt-[32px] bg-${member.pattern} bg-cover rounded-max`}
+                className={`h-[160px] w-[160px] mt-[32px] md:h-[240px] md:w-[240px] md:mt-[32px] bg-${member.pattern} bg-cover rounded-max`}
               ></div>
               <div className="flex flex-col mt-[32px]">
-                <div className="text-brown-2 font-poppins font-bold text-[32px] flex justify-center">
+                <div className="text-brown-2 font-poppins font-bold text-[24px] md:text-[32px] flex justify-center">
                   {member.role}
                 </div>
-                <div className="text-brown-2 font-poppins font-bold text-[23px] flex justify-center">
+                <div className="text-brown-2 font-poppins font-bold md:text-[23px] flex justify-center">
                   {member.name}
                 </div>
               </div>
             </div>
           ))}
-      </motion.div>
+          </Marquee>
     </div>
   );
 }
